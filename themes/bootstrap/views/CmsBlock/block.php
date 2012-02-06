af771be957b256c7a11aa68e0ae561ae29ab3305
niskac <devnull@localhost>
2012-02-06 16:09:12 +0200

    Changed some styles.

--- themes/bootstrap/views/CmsBlock/block.php
+++ themes/bootstrap/views/CmsBlock/block.php
@@ -3,7 +3,7 @@
 	<div class="node-content"><?php echo $content ?></div>
 
 	<?php if (Yii::app()->cms->checkAccess()): ?>
-		<?php echo CHtml::link('<i class="icon-pencil"></i>',
+		<?php echo CHtml::link('<i class="icon-pencil"></i> '.Yii::t('CmsModule.core','Update'),
 				array('/cms/node/update', 'id'=>$model->id), array('class'=>'btn small update-link', 'title'=>Yii::t('CmsModule.core', 'Update'))) ?>
 	<?php endif ?>
 
